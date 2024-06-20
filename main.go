bf234f686fc2ce30d2b7bb519038509d8ef7d3ef
breno-ca <breno_de_avila@outlook.com>
2024-06-20 09:25:27 -0300

    ci: token message check added to main

--- main.go
+++ main.go
@@ -12,5 +12,14 @@ func main() {
 
 	out.PrivateMethod()
 
-	log.Println(os.Getenv("TEST_TOKEN"))
+	token_message := os.Getenv("TEST_TOKEN")
+
+	expected_message := "token de teste, será que consigo ver executando um cat? será que o código consegue usar pra teste?"
+
+	if token_message == expected_message {
+		log.Println("mensagem do token lida com sucesso!")
+		return
+	}
+
+	log.Println("não foi possível ler a mensagem do token")
 }
