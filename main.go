98741286a86736e493bc0666a7f02e577c7deb7f
breno-ca <breno_de_avila@outlook.com>
2024-12-16 13:10:52 -0300

    chore: removed unused code on main

--- main.go
+++ main.go
@@ -11,15 +11,4 @@ func main() {
 	out := private.PrivateRepoType{}
 
 	out.PrivateMethod()
-
-	token_message := os.Getenv("TEST_TOKEN")
-
-	expected_message := "token de teste, será que consigo ver executando um cat? será que o código consegue usar pra teste?"
-
-	if token_message == expected_message {
-		log.Println("mensagem do token lida com sucesso!")
-		return
-	}
-
-	log.Println("não foi possível ler a mensagem do token")
 }
